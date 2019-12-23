df4b073956770bc31e9fe9e1448fc1035a18d521
Ames1977 <56039908+Ames1977@users.noreply.github.com>
2019-12-23 14:43:29 -0500

    local _  variable.rb

--- variable.rb
+++ variable.rb
@@ -2,3 +2,10 @@
  name: greeting ( ' hello world')
 Def greeting
 Puts " hello world" 
+#!/use/bin/ruby
+p local _variable
+name. = "Aimee"
+age ="35"
+place of birtp lace= " Buffalo" 
+favorite _ color = " black" 
+
